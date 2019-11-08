ec3e33af800e5b8a5e97db85677ee812237515ae
Joao <zambuj@gmail.com>
2019-11-08 03:59:08 +0100

    removed old files

--- build.r
+++
@@ -1,7 +0,0 @@
-rmd_head <- readLines("head.rmd") %>% str_c(collapse = "\n")
-rmd_body <- map2_chr(nrp_active, nrp_descriptions, story_snippet) %>%
-  str_c(collapse = "\n")
-
-writeLines(str_c(rmd_head, rmd_body, collapse = "\n"), "index.rmd")
-rmarkdown::render("index.rmd", output_file = "index.html")
-file.remove("index.rmd")
